0dab5e20e97947dd4f8e4e6f5ee9ae56228547e9
Jerson La Torre <jerson.latorre@gmail.com>
2022-03-01 03:23:40 -0500

    autohide menubar

--- src/main/index.js
+++ src/main/index.js
@@ -3,18 +3,17 @@ import { ipcMain } from 'electron'
 import BrowserWinHandler from './BrowserWinHandler'
 
 const winHandler = new BrowserWinHandler({
-  width: 800,
-  height: 600
-  // resizable: false,
-  // maximizable: false
+	width: 800,
+	height: 600,
+	autoHideMenuBar: true,
 })
 
 winHandler.onCreated((browserWindow) => {
-  winHandler.loadPage('/')
-  // Or load custom url
-  // _browserWindow.loadURL('https://google.com')
+	winHandler.loadPage('/')
+	// Or load custom url
+	// _browserWindow.loadURL('https://google.com')
 })
 
 app.on('window-all-closed', function() {
-  app.quit()
+	app.quit()
 })
