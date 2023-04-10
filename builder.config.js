7da26d8cbd265a487bc9af03416751e300b9d652
Jerson La Torre <jerson.latorre@gmail.com>
2023-04-09 21:33:57 -0500

    icons updated

--- builder.config.js
+++ builder.config.js
@@ -4,25 +4,26 @@ const windowsOS = {
   win: {
     icon: ICONS_DIR + 'win-icon.ico',
     publisherName: 'michal',
-    target: 'nsis'
+    target: 'nsis',
   },
 
   nsis: {
-    differentialPackage: true
-  }
+    differentialPackage: true,
+  },
 }
 
 const linuxOS = {
   linux: {
     icon: ICONS_DIR,
-    target: 'deb'
-  }
+    target: 'deb',
+  },
 }
 
 const macOS = {
   mac: {
     target: 'dmg',
-    icon: ICONS_DIR + 'con.icns'
+    icon: ICONS_DIR + 'con.icns',
+    artifactName: 'Miniboard-0.7.dmg',
   },
   dmg: {
     contents: [
@@ -30,15 +31,15 @@ const macOS = {
         x: 410,
         y: 150,
         type: 'link',
-        path: '/Applications'
+        path: '/Applications',
       },
       {
         x: 130,
         y: 150,
-        type: 'file'
-      }
-    ]
-  }
+        type: 'file',
+      },
+    ],
+  },
 }
 
 module.exports = {
@@ -46,21 +47,21 @@ module.exports = {
   appId: 'com.miniboard.app',
   artifactName: 'setup-${version}.${ext}',
   directories: {
-    output: 'build'
+    output: 'build',
   },
   // default files: https://www.electron.build/configuration/contents
   files: [
     'package.json',
     {
       from: 'dist/main/',
-      to: 'dist/main/'
+      to: 'dist/main/',
     },
     {
       from: 'dist/renderer',
-      to: 'dist/renderer/'
-    }
+      to: 'dist/renderer/',
+    },
   ],
   ...windowsOS,
   ...linuxOS,
-  ...macOS
+  ...macOS,
 }
