7593711a4bf9a04a5001522535e5c41a7510c7b8
Jerson La Torre <jerson.latorre@gmail.com>
2024-04-21 23:15:03 -0500

    more robust implementation of the interactive canvas using states.

--- src/scripts/sketch.js
+++ src/scripts/sketch.js
@@ -2,23 +2,31 @@ import Line from './line'
 import hotkeys from 'hotkeys-js'
 
 export const sketch = (p) => {
-  let backgroundColor = '#111'
-  let brushSize = 3
-  let brushColor
+  // Configuración
+  const config = {
+    backgroundColor: '#111',
+    brushSize: 3,
+    brushColor: '#fff',
+  }
 
-  let canvas
-  let offsetX = 0
-  let offsetY = 0
-  let zoom = 1
-  let zoomCenter
-  let panning = false
-  let prevMouseX
-  let prevMouseY
-
+  // Estado
+  let state = 'none' // 'draw', 'zoom', 'pan'
   let currentLine
   let lines = []
   let deletedLines = []
+  let panStartX = 0
+  let panStartY = 0
+  let zoomStartY = 0
 
+  // Canvas
+  let canvas
+  let transform = {
+    x: 0,
+    y: 0,
+    scale: 1,
+  }
+
+  // Funciones
   const undo = (e) => {
     e.preventDefault()
     if (lines.length > 0) deletedLines.push(lines.pop())
@@ -29,13 +37,80 @@ export const sketch = (p) => {
     if (deletedLines.length > 0) lines.push(deletedLines.pop())
   }
 
+  const getMousePosition = () => {
+    return p.createVector((p.mouseX - transform.x) / transform.scale, (p.mouseY - transform.y) / transform.scale)
+  }
+
+  const handleMousePressed = (event) => {
+    const mousePos = getMousePosition()
+    switch (event.button) {
+      case 0: // Left button
+        state = 'draw'
+        currentLine = new Line(config.brushColor, config.brushSize)
+        currentLine.addPoint(mousePos.x, mousePos.y)
+        lines.push(currentLine)
+        break
+      case 1: // Middle button
+        state = 'pan'
+        panStartX = p.mouseX
+        panStartY = p.mouseY
+        break
+      case 2: // Right button
+        state = 'zoom'
+        zoomStartY = p.mouseY
+        break
+    }
+  }
+
+  const handleMouseMoved = () => {
+    if (state === 'pan' && p.mouseIsPressed) {
+      const dx = p.mouseX - panStartX
+      const dy = p.mouseY - panStartY
+
+      transform.x += dx
+      transform.y += dy
+
+      panStartX = p.mouseX
+      panStartY = p.mouseY
+    } else if (state === 'zoom' && p.mouseIsPressed) {
+      const dy = p.mouseY - zoomStartY
+      const zoomIntensity = 0.01
+
+      let newScale = transform.scale + dy * zoomIntensity
+      newScale = Math.max(0.2, Math.min(5, newScale))
+      const scaleFactor = newScale / transform.scale
+
+      transform.x = p.mouseX - scaleFactor * (p.mouseX - transform.x)
+      transform.y = p.mouseY - scaleFactor * (p.mouseY - transform.y)
+      transform.scale = newScale
+
+      zoomStartY = p.mouseY
+    } else if (state === 'draw' && currentLine) {
+      const mousePos = getMousePosition()
+      currentLine.addPoint(mousePos.x, mousePos.y)
+    }
+  }
+
+  const handleMouseReleased = () => {
+    state = 'none'
+  }
+
+  const handleMouseWheel = (event) => {
+    // Evita el comportamiento predeterminado del navegador
+    event.preventDefault()
+
+    // Configura la intensidad del paneo
+    const panIntensity = 5
+
+    // Calcula el desplazamiento basado en la dirección del scroll
+    transform.x -= event.deltaX * panIntensity
+    transform.y -= event.deltaY * panIntensity
+  }
+
+  // Setup
   p.setup = () => {
     canvas = p.createCanvas(p.windowWidth, p.windowHeight).parent('whiteboard')
     canvas.elt.addEventListener('contextmenu', (e) => e.preventDefault())
-    canvas.elt.addEventListener('blur', () => {
-      panning = false
-      zoomCenter = null
-    })
 
     hotkeys('command+z, ctrl+z', undo)
     hotkeys('command+shift+z, ctrl+shift+z', redo)
@@ -43,129 +118,31 @@ export const sketch = (p) => {
     canvas.mousePressed(handleMousePressed)
     canvas.mouseMoved(handleMouseMoved)
     canvas.mouseReleased(handleMouseReleased)
-    canvas.mouseOut(handleMouseOut)
     canvas.mouseWheel(handleMouseWheel)
   }
 
+  // Draw
   p.draw = () => {
-    p.background(backgroundColor)
-    p.translate(p.width / 2, p.height / 2)
-    p.scale(zoom)
-    p.translate(-p.width / 2 + offsetX, -p.height / 2 + offsetY)
+    p.background(config.backgroundColor)
+
+    p.translate(transform.x, transform.y)
+    p.scale(transform.scale)
 
     lines.forEach((line) => line.draw(p))
     if (currentLine) currentLine.draw(p)
   }
 
+  // Resize
   p.windowResized = () => {
     p.resizeCanvas(p.windowWidth, p.windowHeight)
   }
 
-  const handleMousePressed = () => {
-    // start zoom
-    if (p.mouseButton === p.RIGHT) {
-      zoomCenter = { x: p.mouseX, y: p.mouseY }
-      prevMouseY = p.mouseY
-      prevMouseX = p.mouseX
-      return false
-    }
-
-    // start pan
-    if (p.mouseButton === p.CENTER) {
-      prevMouseY = p.mouseY
-      prevMouseX = p.mouseX
-      panning = true
-      return false
-    }
-
-    // create new line
-    if (!panning && !zoomCenter) {
-      currentLine = new Line(brushColor, brushSize)
-      currentLine.addPoint((p.mouseX - p.width / 2) / zoom + p.width / 2 - offsetX, (p.mouseY - p.height / 2) / zoom + p.height / 2 - offsetY)
-    }
-  }
-
-  const handleMouseMoved = () => {
-    // handle zoom
-    if (p.mouseButton === p.RIGHT && zoomCenter) {
-      const zoomIntensity = 0.005
-      let newZoom = zoom + (p.mouseY - prevMouseY) * zoomIntensity
-      newZoom = p.constrain(newZoom, 0.1, 5)
-
-      const zoomPointX = (zoomCenter.x - p.width / 2 + offsetX) / zoom
-      const zoomPointY = (zoomCenter.y - p.height / 2 + offsetY) / zoom
-
-      offsetX -= zoomPointX * newZoom - zoomPointX * zoom
-      offsetY -= zoomPointY * newZoom - zoomPointY * zoom
-
-      zoom = newZoom
-      prevMouseY = p.mouseY
-      return false
-    }
-
-    // panning
-    if (panning) {
-      offsetX += (p.mouseX - prevMouseX) / zoom
-      offsetY += (p.mouseY - prevMouseY) / zoom
-      prevMouseX = p.mouseX
-      prevMouseY = p.mouseY
-      return false
-    }
-
-    // add point
-    if (currentLine && !panning && !zoomCenter) {
-      currentLine.addPoint((p.mouseX - p.width / 2) / zoom + p.width / 2 - offsetX, (p.mouseY - p.height / 2) / zoom + p.height / 2 - offsetY)
-    }
-  }
-
-  const handleMouseReleased = () => {
-    panning = false
-    zoomCenter = null
-
-    // stop zoom
-    if (p.mouseButton === p.RIGHT) {
-      zoomCenter = null
-      return false
-    }
-
-    // stop pan
-    if (p.mouseButton === p.CENTER) {
-      panning = false
-      return false
-    }
-
-    // finish line
-    if (currentLine && currentLine.points.length > 0 && p.mouseButton === p.LEFT) {
-      lines.push(currentLine)
-      currentLine = null
-    }
-
-    // clear deleted lines
-    deletedLines = []
-  }
-
-  const handleMouseOut = () => {
-    panning = false
-    zoomCenter = null
-
-    // finish line
-    if (currentLine && currentLine.points.length > 0 && p.mouseButton === p.LEFT) {
-      lines.push(currentLine)
-      currentLine = null
-    }
-  }
-
-  const handleMouseWheel = (e) => {
-    offsetX -= e.deltaX / zoom
-    offsetY -= e.deltaY / zoom
-  }
-
+  // Update with props
   p.updateWithProps = (props) => {
-    if (props.eraserColor) backgroundColor = props.eraserColor
+    if (props.eraserColor) config.backgroundColor = props.eraserColor
     if (props.brushColor) {
-      brushColor = props.brushColor
-      if (brushColor === backgroundColor) brushSize = 15
-      else brushSize = 3
+      config.brushColor = props.brushColor
+      config.brushSize = config.brushColor === config.backgroundColor ? 20 : 3
     }
   }
 }
